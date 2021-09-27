ee9ffbb7bb2d15fa0fed78e4dec5cd2033bafdde
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2021-09-27 23:15:27 +0530

    first

--- js/all.js
+++ js/all.js
@@ -1,13 +1,3 @@
-var input = document.querySelector("#phone");
-var iti = intlTelInput(input, {
-  initialCountry: "in",
-  nationalMode: true,
-  autoPlaceholder: "polite",
-  utilsScript:
-    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/16.0.11/js/utils.js"
-});
-window.iti = iti;
-
 function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
