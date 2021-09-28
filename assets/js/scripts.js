63cf3f0273809eceb2581dbb520e702103eab0d2
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2021-09-28 23:20:48 +0530

    first

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // Hide the div
+    $(".tick, .tick2, .tick3").hide();
+    // Show the div after 5s
+    $(".tick").delay(3000).fadeIn(300);
+    $(".tick2").delay(4000).fadeIn(300);
+    $(".tick3").delay(5000).fadeIn(300);
+  });
+
+  $(document).on("click", ".next-btn", function() {
+    $(this)
+      .closest("fieldset")
+      .removeClass("active");
+    $(this)
+      .closest("fieldset")
+      .next("fieldset")
+      .addClass("active");
+  });
+
+  $(document).on("click", ".expand", function() {
+    $(this)
+      .closest(".popup")
+      .addClass("double");
+  });
+
